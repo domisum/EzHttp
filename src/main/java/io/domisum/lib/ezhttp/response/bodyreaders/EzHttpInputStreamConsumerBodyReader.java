e4f75df0435729119e4628572706877ee3b9d315
domisum <dominik.weissenseel@gmail.com>
2020-03-24 00:43:16 +0100

    Added EzHttpInputStreamConsumerBodyReader

---
+++ src/main/java/io/domisum/lib/ezhttp/response/bodyreaders/EzHttpInputStreamConsumerBodyReader.java
@@ -0,0 +1,28 @@
+package io.domisum.lib.ezhttp.response.bodyreaders;
+
+import io.domisum.lib.auxiliumlib.contracts.IoConsumer;
+import io.domisum.lib.auxiliumlib.util.java.annotations.API;
+import io.domisum.lib.ezhttp.response.EzHttpResponseBodyReader;
+import lombok.RequiredArgsConstructor;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@API
+@RequiredArgsConstructor
+public class EzHttpInputStreamConsumerBodyReader implements EzHttpResponseBodyReader<Void>
+{
+
+	// INPUT
+	private final IoConsumer<InputStream> inputStreamAction;
+
+
+	// READ
+	@Override
+	public Void read(InputStream inputStream) throws IOException
+	{
+		inputStreamAction.accept(inputStream);
+		return null;
+	}
+
+}

4e8c7d0781d896b146c267d91ec5d11b97c0873a
domisum <me@domisum.io>
2020-09-22 09:46:26 +0200

    Added constructor for ordered data in EzHttpFormUrlEncodedBody

--- src/main/java/io/domisum/lib/ezhttp/request/bodies/EzHttpFormUrlEncodedBody.java
+++ src/main/java/io/domisum/lib/ezhttp/request/bodies/EzHttpFormUrlEncodedBody.java
@@ -1,31 +1,49 @@
 package io.domisum.lib.ezhttp.request.bodies;
 
 import io.domisum.lib.auxiliumlib.annotations.API;
+import io.domisum.lib.auxiliumlib.datacontainers.tuple.Duo;
 import io.domisum.lib.auxiliumlib.util.StringUtil;
 import io.domisum.lib.ezhttp.request.url.EzUrl;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @API
 public class EzHttpFormUrlEncodedBody
-		extends EzHttpPlaintextBody
+	extends EzHttpPlaintextBody
 {
 	
 	// INIT
 	@API
-	public EzHttpFormUrlEncodedBody(Map<String,String> values)
+	public EzHttpFormUrlEncodedBody(List<Duo<String>> entries)
 	{
-		super(encodeValueMap(values));
+		super(encodeEntries(entries));
 	}
 	
-	private static String encodeValueMap(Map<String,String> values)
+	@API
+	public EzHttpFormUrlEncodedBody(Map<String, String> entries)
+	{
+		super(encodeEntries(entries));
+	}
+	
+	private static String encodeEntries(Map<String, String> entries)
+	{
+		var duoEntries = entries.entrySet().stream()
+			.map(e->new Duo<>(e.getKey(), e.getValue()))
+			.collect(Collectors.toList());
+		
+		return encodeEntries(duoEntries);
+	}
+	
+	private static String encodeEntries(List<Duo<String>> entries)
 	{
 		var keyValuePairs = new ArrayList<String>();
-		for(var entry : values.entrySet())
+		for(var entry : entries)
 		{
-			String escapedKey = EzUrl.escapeString(entry.getKey());
-			String escapedValue = EzUrl.escapeString(entry.getValue());
+			String escapedKey = EzUrl.escapeString(entry.getA());
+			String escapedValue = EzUrl.escapeString(entry.getB());
 			
 			String keyValuePair = escapedKey+"="+escapedValue;
 			keyValuePairs.add(keyValuePair);
